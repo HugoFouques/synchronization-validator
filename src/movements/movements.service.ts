32542f5f350726060986d41b443c11e15a569230
Hugo Fouques <hugo.fouques@gmail.com>
2023-10-02 16:09:25 +0200

    Add validation algorithm

--- src/movements/movements.service.ts
+++ src/movements/movements.service.ts
@@ -4,7 +4,29 @@ import { MovementsValidationDto } from './dto/movement-validation.dto';
 @Injectable()
 export class MovementValidationService {
   validation({ movements, balances }: MovementsValidationDto): boolean {
+    const sortedBalances = balances.sort(
+      (bal1, bal2) => bal1.date.getTime() - bal2.date.getTime(),
+    );
 
-    return true;
-    
+    const res = sortedBalances.map((cur, index) => {
+      const previous = balances[index - 1];
+
+      if (!previous) {
+        return true;
+      }
+
+      const relatedMovements = movements.filter(
+        (m) => previous.date < m.date && m.date < cur.date,
+      );
+
+      const amountSum = relatedMovements.reduce(
+        (balance, currentMov) => balance + currentMov.amount,
+        previous.balance,
+      );
+
+      return amountSum === cur.balance;
+    });
+
+    return res.every((e) => e === true);
+  }
 }
